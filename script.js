6edf3d00490b68f11a0b1b36442303c45cd35d42
shobbanshbn <shobanshbn@gmail.com>
2025-06-03 17:21:50 +0530

    Completed

--- script.js
+++ script.js
@@ -62,12 +62,53 @@ const createTask =() =>{
       <div id=${y}>
       <span class="fw-bolder">${ele.text}</span>
       <span class="fw-bolder">${ele.date}</span>
-      <span class="fw-bolder">${ele.textarea}</span>
+      <P class="fw-bolder">${ele.task}</P>
+      <span class="options">
+      <i onclick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-solid fa-pen-to-square fa-bounce" style="color:#FFD43B"></i>
+      <i  onclick="deleteTask(this); createTask()" class="fa-solid fa-trash-can fa-bounce" style="color:#FFD43B"></i>
+      </span>
       </div>
       `
     )
   })
+  resetForm();
 }
+
+// RESETTING THE FORM AFTER DISPLAY THE TASK
+
+const resetForm = ()=>{
+  textInput.value = "";
+  dateInput .value = "";
+  textarea.value = ""
+}
+
+(()=>{
+  data = JSON.parse(localStorage.getItem("data")) || []
+  createTask();
+}) ();
+
+
+// EDIT FUNCTION FOR CREATED TODO's
+
+const editTask = (e) =>{
+  let result = e.parentElement.parentElement;
+  textInput.value = result.children[0].innerHTML;
+  dateInput.value = result.children[1].innerHTML;
+  textarea.value = result.children[2].innerHTML
+
+  // TO REMOVE THE TASK AFTER EDITED
+  deleteTask(e)
+
+}
+
+// DELETE FUNCTION FOR CRETED TODO
+
+const deleteTask = (e)=>{
+  e.parentElement.parentElement.remove();
+  data.splice( e.parentElement.parentElement.id,1)
+  localStorage.setItem("data",JSON.stringify(data))
+}
+
 
 
 
